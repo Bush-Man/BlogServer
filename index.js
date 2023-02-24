7ccb5371f7bb944290f39daed4c1c6e27c220f77
Bush-Man <91716907+Bush-Man@users.noreply.github.com>
2023-02-24 20:14:38 +0300

    Blog Api
    Crud Rest Api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import AuthRoute from "./Routes/AuthRoute.js";
 import PostRoute from "./Routes/PostRoute.js";
-
+import helmet from "helmet";
 
 const app = express();
 
@@ -13,7 +13,11 @@ dotenv.config();
 
 //middlewares
 app.use(cors());
-
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 mongoose.connect(process.env.MONGO_URL, {
